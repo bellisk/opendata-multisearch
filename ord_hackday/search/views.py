9d0c63d0108d2f450111abb6ae65b7484bcafacf
bellisk <daisywheel22@gmail.com>
2015-06-06 12:51:04 +0200

    Fixed merge conflict.

--- ord_hackday/search/views.py
+++ ord_hackday/search/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
-import datetime
 from ord_hackday.search.models import Portal
 import requests
 import json
+import re
 
 
 def search(request):
     c = {}
     query_string = ''
+    portals = Portal.objects.all()
+    c['portals'] = portals
 
     # Construct query
     if 'query' in request.GET:
+        c['query'] = query_string
         query_string = request.GET['query']
 
     pubfrom = request.GET['pubfrom'] + 'T00:00:00Z' if 'pubfrom' in request.GET else ''
@@ -22,9 +25,8 @@ def search(request):
 
     # Search with query
     if len(query_string) > 0:
-        portals = Portal.objects.all()
-        c['portals'] = portals
         c['results'] = []
+        all_results = []
 
         for portal in portals:
             url = portal.url + '/api/3/action/package_search?q=' + query_string
@@ -32,8 +34,28 @@ def search(request):
             json_result = json.loads(r.text)
 
             if json_result['success']:
+                all_results.extend(json_result['result']['results'])
                 for r in json_result['result']['results']:
                     r['result_url'] = portal.url + '/dataset/' + r['name']
                     c['results'].append(r)
 
+            narrowing_terms = extract_narrowing_terms(all_results)
+            c['narrowing_terms'] = narrowing_terms
+
     return render(request, 'search.html', c)
+
+
+def extract_narrowing_terms(results):
+    titles = [re.sub(r'[«»!?,.()\[\]]', ' ', r['title'].lower()) for r in results]
+    wordcounts = {}
+
+    for t in titles:
+        for w in re.split(r' +', t):
+            if not w in wordcounts:
+                wordcounts[w] = 1
+            else:
+                wordcounts[w] += 1
+
+    counttuples = [(k, v) for k, v in wordcounts.iteritems() if len(k) > 4 and v > 1 and v < len(results) * 0.75]
+
+    return [t[0] for t in sorted(counttuples, key=lambda t: -t[1])][:8]
